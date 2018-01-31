44e97caa3cd8b561d0fa762256e59ce221d96b47
Cami <camjohns@umich.edu>
2018-01-31 10:25:35 -0500

    quesitons

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,19 @@
+import random
+
 question_list = []
 
 def ask_question():
     response = input("What is your question?")
     question_list.append(response)
+
+answer_list = ["it is certain", "It is decidedly so", "Without a doubt",
+"Yes definitely", "You may rely on it","As I see it, yes",
+"Most likely", "Outlook good", "Yes", "Signs point to yes",
+"Reply hazy try again", "Better not tell you now", "Cannot predict now",
+"Concentrate and ask again", "Don't count on it", "My reply is no",
+"My sources say no", "Outlook not so good", "Very doubtful"]
+
+def pick_answer():
+    print(random.choice(answer_list))
+
+pick_answer()
